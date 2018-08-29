c1d39ea73cfd00d94476a9ed8aaaffd455f0f431
sigfedepanela <fedepanela2018>
2018-08-29 11:47:41 -0500

    Area Cosechada Caqueta 2017
    Qgis

---
+++ styles/reaCosechadaHaCaquet2017_1_style.js
@@ -0,0 +1,54 @@
+var size = 0;
+var placement = 'point';
+
+var style_reaCosechadaHaCaquet2017_1 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = feature.get("A.COS.(Ha)");
+    var labelText = "";
+    size = 0;
+    var labelFont = "10.725px \'MS Shell Dlg 2\', sans-serif";
+    var labelFill = "rgba(0, 0, 0, 1)";
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'point';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    if (value > 32.000000 && value <= 169.600000) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(0,0,0,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(237,240,23,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement)
+    })]
+                    } else if (value > 169.600000 && value <= 307.200000) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(0,0,0,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(253,174,97,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement)
+    })]
+                    } else if (value > 307.200000 && value <= 444.800000) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(0,0,0,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(230,0,46,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement)
+    })]
+                    } else if (value > 444.800000 && value <= 582.400000) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(0,0,0,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(233,7,165,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement)
+    })]
+                    } else if (value > 582.400000 && value <= 720.000000) {
+            style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(0,0,0,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(10,110,245,1.0)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement)
+    })]
+                    };
+
+    return style;
+};
